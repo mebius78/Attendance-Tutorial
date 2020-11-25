cc48d8426c41972306675e2df036c69f68b2dfb4
mebius78 <masashi.ohno@gmail.com>
2020-11-25 23:37:51 +0000

    Finish add-basic-info

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   root 'static_pages#top'
-  get 'signup', to: 'users#new'
-  get 'sessions/new'
+  get '/signup', to: 'users#new'
 
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
-  #ログイン機能
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete  'logout', to: 'sessions#destroy'
-  
-  resources :users
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
